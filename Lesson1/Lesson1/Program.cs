5398870961679df1b7716eac42fa8fd530e5aef9
Anton <pessist@rambler.ru>
2021-01-16 20:50:47 +0300

    done home work 1

---
+++ Lesson1/Lesson1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Здравствуйте. Введите Ваше имя: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Привет, {name}, сегодня {DateTime.Now}");
+        }
+    }
+}
